2ef6a948d8e49a9f0b170c78657eb59f627783b2
akasantax <joeylin1998@gmail.com>
2023-11-23 04:22:47 -0500

    minor changes

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,6 @@
 pulseaudio -D
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-cd /home/joey/classical-997/
+cd ~/classical-997/
 rm console.log
 sleep 5
 while python3 update.py | grep -q 'unable to connect'
